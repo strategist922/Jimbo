93be2abd58f110c6e4cdd43343ad3d2038c8b86c
Soroush <hatpick@gmail.com>
2013-05-13 21:32:25 -0700

    Styling

--- public/js/snippetCode.js
+++ public/js/snippetCode.js
@@ -23,7 +23,7 @@ var createEditor = function(id, mode) {
     };
 
     var _editor = CodeMirror.fromTextArea(id, {
-        mode : mode,        
+        mode : mode,
         lineNumbers : true,
         lineWrapping : true,
         extraKeys : {
@@ -32,126 +32,227 @@ var createEditor = function(id, mode) {
         autoCloseTags : true,
         matchBrackets : true,
         autoCloseBrackets : true,
-        theme:"solarized light"                
+        theme : "solarized light"
     });
-        
-    _editor.setOption("readOnly", "nocursor");    
     
+    Inlet(_editor);
+    $(".slider").css('display', 'none');
+    $(".picker").css('display', 'none');
+    
+    _editor.setOption("readOnly", "nocursor");
+    
+
     return _editor;
 }
 var editorInit = function(id, mode, type) {
     var _editor;
-    if(type === 'html'){
-        if(!editors.html){
+    if (type === 'html') {
+        if (!editors.html) {
             _editor = createEditor(id, mode);
             editors.html = _editor;
+        } else {
+            _editor = editors.html;
         }
-        else {
-           _editor = editors.html;  
-        }
-    } else if(type === 'js') {
-        if(!editors.js){
+    } else if (type === 'js') {
+        if (!editors.js) {
             _editor = createEditor(id, mode);
             editors.js = _editor;
+        } else {
+            _editor = editors.js;
         }
-        else {
-           _editor = editors.js;  
-        }
-    } else if(type === 'css') {
-        if(!editors.css){
+    } else if (type === 'css') {
+        if (!editors.css) {
             _editor = createEditor(id, mode);
             editors.css = _editor;
+        } else {
+            _editor = editors.css;
         }
-        else {
-           _editor = editors.css;  
-        }     
     } else {
-        if(!editors.json){
+        if (!editors.json) {
             _editor = createEditor(id, mode);
             editors.json = _editor;
+        } else {
+            _editor = editors.json;
         }
-        else {
-           _editor = editors.json;  
-        }     
-    }    
-    return _editor;       
+    }
+    return _editor;
 }
+function shoutOut(cmd, msg) {
+    var s = cmd + "$" + msg;
+    communicationDoc.shout(s);
+}
+
+function shoutHandler(cmd, msg) {
+    var type;
+    switch(cmd) {
+        case 'on':
+            type = 'success';
+            break;
+        case 'eu':
+            type = 'information';
+            break;
+        case 'es':
+            type = 'warning';
+            break;
+        case 'off':
+            type = 'error';
+            break;
+        default:
+            type = "alert";
+            break
+    }
+    noty({
+        text : msg,
+        template : '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>',
+        type : type,
+        dismissQueue : true,
+        layout : 'bottomLeft',
+        timeout : 2000,
+        closeWith : ['button'],
+        buttons : false
+    });
+}
+
+function initCommunication() {
+    var snippetId = sessionStorage["snippetId"];
+    var connection = sharejs.open(snippetId, "text", function(error, comDoc) {
+        if (error) {
+            console.log("communication " + error);
+            return;
+        }
+
+        communicationDoc = comDoc;
+
+        communicationDoc.on('shout', function(s) {
+            var cmd = s.split("$")[0];
+            var msg = s.split("$")[1];
+
+            shoutHandler(cmd, msg);
+        });
+
+        var message = (currentUser === null) ? "Someone just joined your snippet!" : currentUser + " just joined your snippet!";
+        shoutOut("on$" + message);
+    });
+
+    var status = $("#usernameBadge").get(0);
+
+    var register = function(state, klass, text) {
+        connection.on(state, function() {
+            status.className = 'label label-' + klass;
+            $(status).tooltip({
+                placement : "bottom",
+                title : text
+            });
+        });
+    };
+    
+    $("#snippetnameBadge").tooltip({
+        placement : "bottom",
+        title : "Snippet Name"        
+    });
+
+    register('ok', 'success', 'Online');
+    register('connecting', 'warning', 'Connecting...');
+    register('disconnected', 'important', 'Offline');
+    register('stopped', 'default', 'Error');
+
+}
+
 var initApp = function(id, mode) {
+    var currentUser = null;
+    window.currentUser = currentUser;
+
     changeEditorMode(id, mode, "html");
-    
-    $("#shareButton").tooltip({placement:"bottom", title:"Share your snippet!"});
-    $("#editNameButton").tooltip({placement:"bottom", title:"Edit your snippet name!"});
-    $("#editUsernameButton").tooltip({placement:"bottom", title:"Edit you name!"});
-    
+    initCommunication();
+
+    $("#shareButton").tooltip({
+        placement : "bottom",
+        title : "Share your snippet!"
+    });
+    $("#editNameButton").tooltip({
+        placement : "bottom",
+        title : "Edit your snippet name!"
+    });
+    $("#editUsernameButton").tooltip({
+        placement : "bottom",
+        title : "Edit you name!"
+    });
+
     //TODO:Ask for name
 }
-
 var doc = null;
+var communicationDoc = null;
 
 var changeEditorMode = function(id, mode, type) {
-    var codemirror = editorInit(id, mode, type);    
-        
+    var codemirror = editorInit(id, mode, type);
+
     $(".CodeMirror-wrap").height($("#editorArea").height() - $(".nav.nav-tabs").height() - 1);
-        
-    var snippetId = window.location.hash.substring(1);    
-    sessionStorage.setItem("snippetId", snippetId);        
+
+    var snippetId = window.location.hash.substring(1);
+    sessionStorage.setItem("snippetId", snippetId);
     var docName = snippetId + "-" + type;
-    
+
     window.myCodeMirror = codemirror;
-    //ShareJS    
-    var connection = sharejs.open(docName, "text", function(error, newDoc) {               
+    //ShareJS
+    var connection = sharejs.open(docName, "text", function(error, newDoc) {
         if (doc !== null) {
             doc.close();
             doc.detach_cm();
         }
 
         doc = newDoc;
-        console.log(doc.name)
 
         if (error) {
             console.error(error);
             return;
         }
         doc.attach_cm(myCodeMirror);
-        myCodeMirror.setOption("readOnly", false);                        
-    });                                
-    
+        myCodeMirror.setOption("readOnly", false);
+    });
+
     /*if ($(".CodeMirror.CodeMirror-wrap").size() > 1) {
-        $($(".CodeMirror.CodeMirror-wrap")).remove();
-    }*/        
+     $($(".CodeMirror.CodeMirror-wrap")).remove();
+     }*/
 }
 
 window.onload = function() {
     layout();
-    var elem = document.getElementById("htmlEditor");    
+    var elem = document.getElementById("htmlEditor");
     initApp(elem, "text/html");
 }
 
+window.onunload = function() {
+    if (communicationDoc !== null) {
+        var message = (currentUser === null) ? "Someone just left your snippet!" : currentUser + " just left your snippet!";
+        shoutOut("off$" + message);
+        //communicationDoc.close();
+    }
+}
 var currentTabGlobal = "";
 
-$(document).ready(function() {    
-        
-    $(".nav-tabs>li").on('click', function(e) {        
+$(document).ready(function() {
+
+    $(".nav-tabs>li").on('click', function(e) {
         var currentEditor = e.currentTarget.dataset["id"];
-        if(currentTabGlobal === currentEditor) {            
-            return;            
+        if (currentTabGlobal === currentEditor) {
+            return;
         }
         currentTabGlobal = currentEditor;
         switch(currentEditor) {
             case 'htmlTab':
                 var _id = document.getElementById("htmlEditor");
                 var _mode = "text/html";
-                changeEditorMode(_id, _mode, "html");                                
+                changeEditorMode(_id, _mode, "html");
                 break;
             case 'jsTab':
                 var _id = document.getElementById("jsEditor");
                 var _mode = "text/javascript";
-                changeEditorMode(_id, _mode, "js");                
+                changeEditorMode(_id, _mode, "js");
                 break;
             case 'cssTab':
                 var _id = document.getElementById("cssEditor");
                 var _mode = "text/css";
-                changeEditorMode(_id, _mode, "css");                
+                changeEditorMode(_id, _mode, "css");
                 break;
             case 'jsonTab':
                 var _id = document.getElementById("jsonEditor");
@@ -159,10 +260,46 @@ $(document).ready(function() {
                     name : "javascript",
                     json : true
                 };
-                changeEditorMode(_id, _mode, "json");                
+                changeEditorMode(_id, _mode, "json");
                 break;
             default:
                 break;
         }
+    });
+
+    var shareSnippet = function() {
+        var dialogHeader = "<button type='button' class='close' data-dismiss='modal'>×</button><p class='center'><i class='icon icon-user icon-orange'></i> Share Snippet via Email</p>";
+        var dialogContent = '<input type="email" id="email" placeholder="Enter a valid email!" width="100%" required/>';
+
+        var dialogFooter = $("<div>").append($("<button>").addClass("btn btn-info").text("Share").click(function() {
+            //Send email with link inside
+            $("#dialog").modal('hide');
+        }).attr('type', 'submit')).append($("<button>").attr("data-dismiss", "modal").addClass("btn btn-primary").text("Cancel").css('margin', '5px 5px 6px'));
+
+        $("#dialog>div.modal-header").html(dialogHeader);
+        $("#dialog>div.modal-body").html(dialogContent);
+        $("#dialog>div.modal-footer").html(dialogFooter);
+
+        $("#dialog").modal("show");
+    }
+    
+    $("#usernameBadge").editable({
+        type : 'text',                
+        name: "usernameBadge",
+        placement: 'bottom',
+        value : $("#usernameBadge").val()        
+    });    
+    
+    $("#snippetnameBadge").editable({
+        type : 'text',        
+        autotext : "never",         
+        name: "usernameBadge",
+        placement: 'bottom',
+        value : $("#snippetnameBadge").val()               
+    });    
+   
+    //Share snippet via email
+    $("a[data-action=shareButton]").click(function() {
+        shareSnippet();
     });
 });
---
+++ public/thirdparty/CodeMirrorEditor/inlet.js
@@ -0,0 +1,119 @@
+var Inlet = (function() {
+    function inlet(ed) {
+        var editor = ed;
+        var slider;        
+        var picker;
+        
+        var wrapper = editor.getWrapperElement();
+        $(wrapper).on("mousedown", onClick);
+
+        
+        if($(".inlet_slider").size() === 0){
+            var slider_node = document.createElement("div");
+            slider_node.className = "inlet_slider";
+            $("#editorArea").append(slider_node);
+            slider = $(slider_node);
+            slider.slider();
+            slider.css('visibility', 'hidden');                
+        } else {
+            slider = $(".inlet_slider");
+            slider.css('visibility', 'hidden');
+        }
+        
+        if($("#colorPicker").size() !== 0) {            
+            $("#colorPicker").remove();
+             
+        }        
+        picker = new Color.Picker({
+            id: "colorPicker",
+            color: "#643263",
+            display: false,
+            size: 150,
+            callback: function(rgba, state, type) {           
+                var newcolor = Color.Space(rgba, "RGB>STRING");
+
+                var cursor = editor.getCursor();
+                var token = editor.getTokenAt(cursor);
+
+                var start = {
+                    "line" : cursor.line,
+                    "ch" : token.start
+                };
+                var end = {
+                    "line" : cursor.line,
+                    "ch" : token.end
+                };
+                start.ch = start.ch + token.string.indexOf("#");
+                end.ch = start.ch + 7;
+
+                editor.replaceRange('#' + newcolor.toUpperCase(), start, end);
+            }
+        });
+        
+        
+        function onClick(ev) {
+            var cursor = editor.getCursor(true);
+            var token = editor.getTokenAt(cursor);
+            cursorOffset = editor.cursorCoords(true, "page");          
+                        
+            if(token.type === "number") {
+                var value = parseFloat(token.string);
+                var sliderRange;
+
+                if (value === 0) { 
+                    sliderRange = [-100, 100];
+                } else {
+                    sliderRange = [-value * 3, value * 5];
+                }
+
+                var slider_min = _.min(sliderRange);
+                var slider_max = _.max(sliderRange);
+                slider.slider('option', 'min', slider_min);
+                slider.slider('option', 'max', slider_max);
+
+                if ((slider_max - slider_min) > 20) {
+                    slider.slider('option', 'step', 1);
+                } else {
+                    slider.slider('option', 'step', (slider_max - slider_min)/200);
+                }
+                slider.slider('option', 'value', value);
+
+                var y_offset = 15;
+                var sliderTop = cursorOffset.top - y_offset;
+                var sliderLeft = cursorOffset.left - slider.width()/2;
+
+                slider.offset({top: sliderTop - 10, left: sliderLeft});
+                
+                slider.slider('option', 'slide', function(event, ui) {
+                    var mcursor = editor.getCursor(true);
+                    var mtoken = editor.getTokenAt(cursor);                     
+                    var start = {"line":mcursor.line, "ch":mtoken.start};
+                    var end = {"line":mcursor.line, "ch":mtoken.end};                    
+                    editor.replaceRange(String(ui.value.toFixed(2)), start, end);
+                });
+
+                slider.css('visibility', 'visible');
+                picker.element.style.display = "none";
+            
+            } else {                
+                var match = token.string.match(/#+(([a-fA-F0-9]){3}){1,2}/);                
+                if(match) {                    
+                    var color = match[0];
+                    color = color.slice(1, color.length);
+                    picker.update(color);
+                    
+                    var top = cursorOffset.y + "px";
+                    var left = cursorOffset.x + 100 + "px";
+                    $("#ColorPicker").css('position', "absolute");
+                    $("#ColorPicker").css('top', top);
+                    $("#ColorPicker").css('left', left);                    
+                    picker.toggle(true);
+                } else {                    
+                    picker.toggle(false);                                        
+                }                
+                slider.css('visibility', 'hidden');
+            }
+        }
+    }
+    return inlet;
+})();
