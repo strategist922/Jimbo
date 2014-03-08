a24d153484c0c782e9d9faf5fa5d52aa713d1bd2
Soroush <hatpick@gmail.com>
2014-03-07 17:16:31 -0800

    test

--- public/js/reviewCode.js
+++ public/js/reviewCode.js
@@ -73,6 +73,7 @@ $(document).ready(function(){
 		     else {
 		       final.push(tempIns);
 		       tempIns.ops = [];
+		       continue;
 		     }
 		   }
 		  } else if(op.op[0].hasOwnProperty("d")){
@@ -85,6 +86,7 @@ $(document).ready(function(){
 		     else {
 		       final.push(tempDel);
 		       tempDel.ops = [];
+		       continue;
 		     }
 		   }
 		  }
