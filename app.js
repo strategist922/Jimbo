eb227825279aaf44fb8517a54df789bc7bcf01d3
Soroush <hatpick@gmail.com>
2014-04-07 15:02:38 -0700

    change db prefix for study

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.configure('production', function(){
 require('./routes/index')(app)
 
 var options = {/*db: {type: 'redis'}*/}; // See docs for options. {type: 'redis'} to enable persistance.
-  options.db = {type: 'redis', prefix: 'UserStudy'};
+  options.db = {type: 'redis', prefix: 'DataVisStudy:'};
   options.auth = function(agent, action) {
   action.accept();
 };
